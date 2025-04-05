b9831d340816822eaa81f7f6f5309cbce46496b4
athul-narayanan <athulnarayanan62@gmail.com>
2025-04-05 16:56:29 -0400

    Quick update added

--- user/urls.py
+++ user/urls.py
@@ -7,10 +7,12 @@ from user.views.authenticationview import AuthenticationView
 from user.views.userscheduleview import UserScheduleView
 from user.views.userview import CreateUserView
 from user.views.PIngAPIView import PingAPIView
+from user.views.QuickUpdateView import QuickUpdateView
 
 urlpatterns = [
     path('create/', CreateUserView.as_view()),
     path('login/', AuthenticationView.as_view()),
     path('schedule', UserScheduleView.as_view()),
-    path('ping', PingAPIView.as_view())
+    path('ping', PingAPIView.as_view()),
+    path('quickupdate', QuickUpdateView.as_view())
 ]
--- user/views/QuickUpdateView.py
+++ user/views/QuickUpdateView.py
@@ -1,5 +1,5 @@
 """
-    View to update node mcu is ready to receive message
+
 """
 
 from rest_framework import generics
@@ -9,37 +9,51 @@ from user.models import User
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.permissions import AllowAny
+import requests
 
 
-class PingAPIView(generics.CreateAPIView):
-    permission_classes = [AllowAny] # Bypass JWT for /create API
+class QuickUpdateView(generics.CreateAPIView):
     """
-        This view is used to update ping from node mcu
+        This view is perform quick update of schedule
     """
     @extend_schema(
     parameters=[
         OpenApiParameter(
-            name='email',
+            name='message',
             type=OpenApiTypes.STR,
             location=OpenApiParameter.QUERY,
             description='',
-            required=False
+            required=True
         ),
     ])
     
     def post(self, request):
         try:
-            email = request.query_params.get('email')
-            if email is None:
+            message = request.query_params.get('message')
+            if message is None:
                 return Response({
                     'error': 'Invalid request',
                 }, status=status.HTTP_400_BAD_REQUEST)
         
-            user = User.objects.get(email=email)
-            user.ping_started = True
+            user = User.objects.get(email=request.user.email)
+            user.ping_started = False
             user.save()
+            message = message.replace(" ", "+")
+            message = message.ljust(16, "+")
+            name = user.firstname + "+" + user.lastname
+            name = name.ljust(16,"+")
+            
+            # Call node MCU API and update the message
+            url = f'http://10.8.4.100/updateMessage?message={message}&name={name}'
+            print(url)
+            headers = {
+                'Content-Type': 'application/json',
+            }
+
+            requests.get(url, headers=headers)
+
             return Response({
-                'message': "Ping Successful"
+                'message': "Quick update successful"
             }, status=status.HTTP_200_OK)
 
         except User.DoesNotExist:
